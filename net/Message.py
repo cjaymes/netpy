b2a2f1a436e39a798452fe78d3d78ef41a2b3713
Casey Jaymes <cjaymes@gmail.com>
2016-10-16 11:43:46 -0400

    up to pstring

--- net/Message.py
+++ net/Message.py
@@ -17,7 +17,9 @@
 
 import inspect
 import struct
+import logging
 
+logger = logging.getLogger(__name__)
 class Message():
     BYTE_ORDER_NATIVE = 0
     BYTE_ORDER_LITTLE_ENDIAN = 1
@@ -29,41 +31,96 @@ class Message():
         self._byte_offset = 0
         self._bit_offset = 0
 
+    def __set_byte_offset(self, off):
+        if off > len(self._buffer):
+            raise IndexError('Tried skipping beyond end of buffer; len ' + str(len(self._buffer)))
+
+        self._byte_offset = off
+
+    def __set_bit_offset(self, off):
+        if int(off / 8) > 0:
+            new_byte_offset = self._byte_offset + int(off / 8)
+            if new_byte_offset > len(self._buffer):
+                raise IndexError('Tried skipping beyond end of buffer; len ' + str(len(self._buffer)))
+            new_bit_offset = off % 8
+            if new_byte_offset == len(self._buffer) \
+                and new_bit_offset > 0:
+                raise IndexError('Tried skipping beyond end of buffer; len ' + str(len(self._buffer)))
+
+            self._byte_offset = new_byte_offset
+            self._bit_offset = new_bit_offset
+        else:
+            new_bit_offset = off
+            if self._byte_offset == len(self._buffer) \
+                and new_bit_offset > 0:
+                raise IndexError('Tried skipping beyond end of buffer; len ' + str(len(self._buffer)))
+
+            self._bit_offset = new_bit_offset
+
     def __align(self):
         if self._bit_offset != 0:
-            self._bit_offset = 0
-            self._byte_offset += 1
+            self.__set_bit_offset(0)
+            self.__set_byte_offset(self._byte_offset + 1)
 
-    def _skip_padding_bytes(self, length):
+    def _skip_bytes(self, length):
         self.__align()
-        self._byte_offset += length
+        self.__set_byte_offset(self._byte_offset + length)
 
-    def _skip_padding_bits(self, length):
-        if int(length / 8) > 0:
-            self._byte_offset += int(length / 8)
-            self._bit_offset += length % 8
-        else:
-            self._bit_offset += length
+    def _skip_bits(self, length):
+        self.__set_bit_offset(self._bit_offset + length)
 
-    def _unpack_bytes(self, count):
+    def _unpack_bytes(self, length):
         self.__align()
 
         start_offset = self._byte_offset
-        end_offset = self._byte_offset + count
+        end_offset = self._byte_offset + length
 
         value = self._buffer[start_offset:end_offset]
 
-        self._byte_offset = end_offset
+        self.__set_byte_offset(end_offset)
 
         return value
 
-    def _unpack_string(self, count=1):
+    def _unpack_string(self, length):
         self.__align()
-        pass
 
-    def _unpack_sized_string(self):
+        fmt = '=' + str(length) + 's'
+        logger.debug('Format: ' + fmt)
+
+        try:
+            (value,) = struct.unpack_from(fmt, self._buffer, self._byte_offset)
+        except struct.error as e:
+            raise IndexError(e)
+        logger.debug('Unpacked: ' + str(value))
+
+        value = value.decode()
+        logger.debug('Decoded: ' + value)
+
+        self.__set_byte_offset(self._byte_offset + length)
+
+        return value
+
+    def _unpack_pascal_string(self):
         self.__align()
-        pass
+
+        try:
+            (value,) = struct.unpack_from('=B', self._buffer, self._byte_offset)
+        except struct.error as e:
+            raise IndexError(e)
+        logger.debug('Unpacked: ' + str(value))
+
+        try:
+            (value,) = struct.unpack_from('=' + str(value + 1) + 'p', self._buffer, self._byte_offset)
+        except struct.error as e:
+            raise IndexError(e)
+        logger.debug('Unpacked: ' + str(value))
+
+        value = value.decode()
+        logger.debug('Decoded: ' + value)
+
+        self.__set_byte_offset(self._byte_offset + len(value) + 1)
+
+        return value
 
     def _unpack_terminated_string(self, terminator='\0'):
         self.__align()
@@ -73,6 +130,10 @@ class Message():
         pass
 
     def _unpack_unsigned_integer(self, bits=8, byte_order=BYTE_ORDER_NATIVE):
+        logger.debug('Capturing ' + str(bits) + ' in byte order ' + str(byte_order))
+        logger.debug('Initial offsets: bit: ' + str(self._bit_offset) \
+            + ', byte: ' + str(self._byte_offset))
+
         if byte_order == Message.BYTE_ORDER_NATIVE:
             fmt = '='
         elif byte_order == Message.BYTE_ORDER_LITTLE_ENDIAN:
@@ -81,34 +142,53 @@ class Message():
             fmt = '>'
         else:
             raise ValueError('Unknown byte order: ' + str(byte_order))
+        logger.debug('Format after byte order: ' + fmt)
 
+        value_width = None
         if bits <= 0:
             raise ValueError('Cannot unpack <= 0 bit integer value')
         elif bits <= 8:
             fmt += 'B'
+            value_width = 8
+            mask = 0xff
         elif bits <= 16:
             fmt += 'H'
+            value_width = 16
+            mask = 0xffff
         elif bits <= 32:
             fmt += 'I'
+            value_width = 32
+            mask = 0xffffffff
         elif bits <= 64:
             fmt += 'Q'
+            value_width = 64
+            mask = 0xffffffffffffffff
         else:
             raise ValueError('Cannot unpack integer value greater than 64 bits (try float/double)')
+        logger.debug('Format after bits: ' + fmt)
+        bin_fmt = '0' + str(value_width) + 'b'
 
-        (value,) = struct.unpack_from(fmt, self._buffer, self._byte_offset)
+        try:
+            (value,) = struct.unpack_from(fmt, self._buffer, self._byte_offset)
+        except struct.error as e:
+            raise IndexError(e)
+        logger.debug('Unpacked value:                   0b' + format(value, bin_fmt) + '(' + hex(value) + ')')
 
         if self._bit_offset != 0:
             # mask off the offset bits
-            mask = 0xFF >> (8 - self._bit_offset)
-            value = value & mask
+            mask = mask >> self._bit_offset
+            logger.debug('Offset mask:                      0b' + format(mask, bin_fmt) + '(' + hex(mask) + ')')
+        value = value & mask
+        logger.debug('Value after bit offset masking:   0b' + format(value, bin_fmt) + '(' + hex(value) + ')')
 
-        remaining_bits = 8 - (self._bit_offset + (bits % 8))
+        remaining_bits = value_width - (self._bit_offset + (bits % 8))
         value = value >> remaining_bits
+        logger.debug('Value after shifting:             0b' + format(value, bin_fmt) + '(' + hex(value) + ')')
 
-        self._bit_offset += bits
-        if int(self._bit_offset / 8) > 0:
-            self._byte_offset += int(self._bit_offset / 8)
-            self._bit_offset = self._bit_offset % 8
+        self.__set_bit_offset(self._bit_offset + bits)
+
+        logger.debug('Final offsets: bit: ' + str(self._bit_offset) \
+            + ', byte: ' + str(self._byte_offset))
 
         return value
 
--- tests/net/test_Message.py
+++ tests/net/test_Message.py
@@ -18,37 +18,53 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath("."))
+import logging
+logging.basicConfig(level=logging.DEBUG)
+import pytest
 
 from net.Message import Message
 
-def test_skip_padding_bytes_without_bits():
+################ Skipping ################
+def test_skip_bytes_without_bits():
     msg = Message(b'\x00\x00\x33')
-    msg._skip_padding_bytes(2)
+    msg._skip_bytes(2)
     assert(msg._unpack_bytes(1) == b'\x33')
 
-def test_skip_padding_bytes_with_bits():
+def test_skip_bytes_with_bits():
     msg = Message(b'\x00\x00\x33')
-    msg._skip_padding_bits(4)
+    msg._skip_bits(4)
     assert(msg._byte_offset == 0)
     assert(msg._bit_offset == 4)
-    msg._skip_padding_bytes(1)
+    msg._skip_bytes(1)
     assert(msg._byte_offset == 2)
     assert(msg._bit_offset == 0)
     assert(msg._unpack_bytes(1) == b'\x33')
 
+def test_skip_bytes_beyond_end():
+    msg = Message(b'\x00\x00\x33')
+    with pytest.raises(IndexError):
+        msg._skip_bytes(9)
+
 def test_skip_bits_within_byte():
     msg = Message(b'\x13')
-    msg._skip_padding_bits(4)
+    msg._skip_bits(4)
     assert(msg._byte_offset == 0)
     assert(msg._bit_offset == 4)
     assert(msg._unpack_unsigned_integer(bits=4) == 3)
 
-# def test_skip_bits_beyond_byte():
-#     assert(False)
-#
-# def test_pack_bytes():
-#     assert(False)
-#
+def test_skip_bits_between_byte():
+    msg = Message(b'\x12\x34\x56\x78\x9A')
+    msg._skip_bits(9)
+    assert(msg._byte_offset == 1)
+    assert(msg._bit_offset == 1)
+    assert(msg._unpack_unsigned_integer(bits=7) == 0x34)
+
+def test_skip_bits_beyond_end():
+    msg = Message(b'\x12')
+    with pytest.raises(IndexError):
+        msg._skip_bits(9)
+
+################ Unpacking ################
 def test_unpack_bytes_1():
     msg = Message(b'\x33')
     assert(msg._unpack_bytes(1) == b'\x33')
@@ -57,125 +73,137 @@ def test_unpack_bytes_3():
     msg = Message(b'\x11\x22\x33')
     assert(msg._unpack_bytes(3) == b'\x11\x22\x33')
 
-# def test_pack_string():
-#     assert(False)
-#
-# def test_unpack_string():
-#     assert(False)
-#
-# def test_pack_string_beyond_end():
-#     assert(False)
-#
-# def test_unpack_string_beyond_end():
-#     assert(False)
-#
-# def test_pack_sized_string():
-#     assert(False)
-#
-# def test_unpack_sized_string():
-#     assert(False)
-#
-# def test_pack_sized_string_beyond_end():
-#     assert(False)
-#
-# def test_unpack_sized_string_beyond_end():
-#     assert(False)
-#
+def test_unpack_string():
+    msg = Message(b'\x48\x65\x6c\x6c\x6f\x33')
+    assert(msg._unpack_string(5) == 'Hello')
+
+def test_unpack_string_beyond_end():
+    msg = Message(b'\x48\x65\x6c\x6c\x6f\x33')
+    with pytest.raises(IndexError):
+        msg._unpack_string(9)
+
+def test_unpack_pascal_string():
+    msg = Message(b'\x05\x48\x65\x6c\x6c\x6f\x33')
+    assert(msg._unpack_pascal_string() == 'Hello')
+
+def test_unpack_pascal_string_beyond_end():
+    msg = Message(b'\x09\x48\x65\x6c\x6c\x6f\x33')
+    with pytest.raises(IndexError):
+        msg._unpack_pascal_string()
+
 # def test_unpack_terminated_string():
 #     assert(False)
 #
-# def test_pack_terminated_string():
-#     assert(False)
-#
-# def test_unpack_terminated_string_beyond_end():
-#     assert(False)
-#
-# def test_pack_big_endian_byte_order():
+# def test_unpack_unterminated_string():
 #     assert(False)
 #
 # def test_unpack_big_endian_byte_order():
 #     assert(False)
 #
-# def test_pack_little_endian_byte_order():
-#     assert(False)
-#
 # def test_unpack_little_endian_byte_order():
-#     assert(False)
-#
-# def test_pack_native_byte_order():
 #     assert(False)
 #
 # def test_unpack_native_byte_order():
 #     assert(False)
 #
-# def test_pack_signed_integer_8bit():
-#     assert(False)
-#
 # def test_unpack_signed_integer_8bit():
-#     assert(False)
-#
-# def test_pack_signed_integer_16bit():
 #     assert(False)
 #
 # def test_unpack_signed_integer_16bit():
 #     assert(False)
 #
-# def test_pack_signed_integer_32bit():
-#     assert(False)
-#
 # def test_unpack_signed_integer_32bit():
-#     assert(False)
-#
-# def test_pack_signed_integer_64bit():
 #     assert(False)
 #
 # def test_unpack_signed_integer_64bit():
 #     assert(False)
 #
-# def test_pack_unsigned_integer_8bit():
-#     assert(False)
-#
 # def test_unpack_unsigned_integer_8bit():
-#     assert(False)
-#
-# def test_pack_unsigned_integer_16bit():
 #     assert(False)
 #
 # def test_unpack_unsigned_integer_16bit():
 #     assert(False)
 #
-# def test_pack_unsigned_integer_32bit():
-#     assert(False)
-#
 # def test_unpack_unsigned_integer_32bit():
-#     assert(False)
-#
-# def test_pack_unsigned_integer_64bit():
 #     assert(False)
 #
 # def test_unpack_unsigned_integer_64bit():
 #     assert(False)
 #
-# def test_pack_bool_8bit():
-#     assert(False)
-#
 # def test_unpack_bool_8bit():
-#     assert(False)
-#
-# def test_pack_bool_1bit():
 #     assert(False)
 #
 # def test_unpack_bool_1bit():
 #     assert(False)
 #
-# def test_pack_float():
+# def test_unpack_float():
 #     assert(False)
 #
-# def test_unpack_float():
+# def test_unpack_double():
+#     assert(False)
+
+################ Packing ################
+
+# def test_pack_bytes():
+#     assert(False)
+#
+# def test_pack_string():
+#     assert(False)
+#
+# def test_pack_string_beyond_end():
+#     assert(False)
+#
+# def test_pack_sized_string():
+#     assert(False)
+#
+# def test_pack_sized_string_beyond_end():
+#     assert(False)
+#
+# def test_pack_terminated_string():
+#     assert(False)
+#
+# def test_pack_big_endian_byte_order():
+#     assert(False)
+#
+# def test_pack_little_endian_byte_order():
+#     assert(False)
+#
+# def test_pack_native_byte_order():
+#     assert(False)
+#
+# def test_pack_signed_integer_8bit():
+#     assert(False)
+#
+# def test_pack_signed_integer_16bit():
+#     assert(False)
+#
+# def test_pack_signed_integer_32bit():
+#     assert(False)
+#
+# def test_pack_signed_integer_64bit():
+#     assert(False)
+#
+# def test_pack_unsigned_integer_8bit():
+#     assert(False)
+#
+# def test_pack_unsigned_integer_16bit():
+#     assert(False)
+#
+# def test_pack_unsigned_integer_32bit():
+#     assert(False)
+#
+# def test_pack_unsigned_integer_64bit():
+#     assert(False)
+#
+# def test_pack_bool_8bit():
+#     assert(False)
+#
+# def test_pack_bool_1bit():
+#     assert(False)
+#
+# def test_pack_float():
 #     assert(False)
 #
 # def test_pack_double():
 #     assert(False)
 #
-# def test_unpack_double():
-#     assert(False)
