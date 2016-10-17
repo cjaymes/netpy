2524528e952325f69f1262c1064c1a4db3e42af1
Casey Jaymes <cjaymes@gmail.com>
2016-10-17 17:15:38 -0400

    implemented format label

--- net/dcerpc/ndr/FormatLabel.py
+++ net/dcerpc/ndr/FormatLabel.py
@@ -15,12 +15,36 @@
 # You should have received a copy of the GNU General Public License
 # along with NetPy.  If not, see <http://www.gnu.org/licenses/>.
 
+from bitstring import BitStream
+import logging
+
 from net.Structure import Structure
 
+logger = logging.getLogger(__name__)
 class FormatLabel(Structure):
+    FORMAT_CHAR_ASCII = 0
+    FORMAT_CHAR_EBCDIC = 1
+
+    BYTE_ORDER_BIG_ENDIAN = 0
+    BYTE_ORDER_LITTLE_ENDIAN = 1
+
+    FORMAT_FLOAT_IEEE   = 0
+    FORMAT_FLOAT_VAX    = 1
+    FORMAT_FLOAT_CRAY   = 2
+    FORMAT_FLOAT_IBM    = 3
+
     @staticmethod
     def from_bytes(buf):
-        raise NotImplementedError(inspect.stack()[0][3] + '() has not been implemented in subclass: ' + self.__class__.__name__)
+        bs = BitStream(bytes=buf)
+        lbl = FormatLabel()
+
+        lbl.int_repr = bs.read('uint:4')
+        lbl.char_repr = bs.read('uint:4')
+        lbl.float_repr = bs.read('uint:8')
+        bs.read('uint:8')   # reserved
+        bs.read('uint:8')   # reserved
 
     def to_bytes():
-        raise NotImplementedError(inspect.stack()[0][3] + '() has not been implemented in subclass: ' + self.__class__.__name__)
+        bs = bitstring.pack('uint:4, uint:4, uint:8, 0xFF, 0xFF', \
+            self.int_repr, self.char_repr, self.float_repr)
+        return bs.tobytes()
